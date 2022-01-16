749cdb1c4f9d958402bc794fe0cdc6faa8ae4152
jyy1554 <jyy1556@gmail.com>
2022-01-16 23:44:51 +0900

    이미지를 포함한 게시글 올리기

--- public/client/src/components/MainFeed/MainFeed.jsx
+++ public/client/src/components/MainFeed/MainFeed.jsx
@@ -1,16 +1,48 @@
-import React, { useCallback, useState } from 'react'
+import firebaseApp from '@config/firebaseApp';
+import React, { useCallback, useState, useRef } from 'react'
 import { useSelector } from 'react-redux';
 import './css/index.css'
 
+const Fstorage = firebaseApp.storage();
+
+function uploadImageToFirebaseStorage(data, timestamp) {
+  return new Promise((resolve, reject) => {
+    Fstorage.ref(`feed/${timestamp}/feed.jpg`)
+      .putString(data.split(",")[1], 'base64', {
+        contentType : 'image/jpg'
+      })
+      .then(snapshot => {
+        snapshot.ref.getDownloadURL().then((url) => {
+          console.log('이미지 업로드 완료');
+          resolve(url);
+        }).catch(err => {
+          reject(err)
+        });
+      }).catch(err => {
+        reject(err);
+      });
+  });
+}
+
 function MainFeed() {
+  const contextRef = useRef(null);
   const session = useSelector(state => state.auth.session);
   const [context, setContext] = useState(undefined);
   const [feed_image, setFeed_image] = useState(undefined);
 
   const __makeFeed = useCallback(
-    (e) => {
+    async(e) => {
       e.preventDefault();
-      if (session) {
+      if (session && (context || feed_image)) {
+        const nowTime = Date.now();
+        let downloadUrl;
+
+        if (feed_image) {
+          //파이어베이스 스토리지에 이미지를 업로드한뒤, URL을 받아서 FETCH로 넘겨줘야함
+          downloadUrl = await uploadImageToFirebaseStorage(feed_image, nowTime).catch(err => {
+            console.log(err);
+          });
+        }
         const {uid} = session;
         let url = '/feed/new';
 
@@ -24,23 +56,44 @@ function MainFeed() {
             body: JSON.stringify({
               feed : {
                 context,
-
+                image : downloadUrl,
+                like : 0,
               },
               profile : {
                 uid
-              }
+              },
+              timestamp : nowTime
             })
           })
             .then(res => res.json())
             .then(({msg}) => {
+              contextRef.current.value = '';
+              setContext(undefined);
+              setFeed_image(undefined);
+              alert(msg);
               console.log(msg);
             }).catch(err => {
               console.log(err);
             });
       }
     },
-    [context, feed_image, session]
+    [context, feed_image, session, contextRef]
   );
+
+  const __getData64FromImage = useCallback(
+    (e) => {
+      const fileList = e.target.files[0];
+        
+      const reader = new FileReader();
+
+      reader.onload = (e) => {
+        setFeed_image(e.target.result);
+      };
+
+      reader.readAsDataURL(fileList);
+    },
+    [],
+  )
 
   return (
     <div className='mainfeed'>
@@ -51,13 +104,20 @@ function MainFeed() {
 
             </div>
             <div className='inp'>
-              <input type="text" placeholder='오늘 무슨일이 있었나요?' onChange={(e) => setContext(e.target.value)} />
+              <input 
+                ref={contextRef}
+                type="text"
+                placeholder='오늘 무슨일이 있었나요?'
+                onChange={(e) => setContext(e.target.value)} />
             </div>
             <div className='get-image'>
               <label htmlFor="get-image-input">
                 <img src="/assets/main/add-image.svg" alt="이미지 추가하기" />
               </label>
-              <input id='get-image-input' type="file" />
+              <input 
+                id='get-image-input'
+                type="file"
+                onChange={__getData64FromImage} />
             </div>
           </form>
 
